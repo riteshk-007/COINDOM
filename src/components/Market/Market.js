aceb265c547cb73cf7db13d9c6ed7694913d03c7
Ritesh <codeshorts007@gmail.com>
2023-09-08 14:44:47 +0530

    style- single page design

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,7 +1,34 @@
 import React from "react";
 import "./Market.scss";
+import { Link } from "react-router-dom";
 function Market() {
-  return <div className="market"></div>;
+  /* https://react-responsive-pagination.elantha.com/custom-styled-pagination/ */
+
+  return (
+    <div className="market">
+      <h1>Market Update</h1>
+      <div className="container">
+        <div className="table">
+          <div className="head">
+            <p>Coin</p>
+            <p>Price</p>
+            <p>24h Change</p>
+            <p>Market Cap</p>
+          </div>
+          <Link className="coinDetail link" to={"/coin/1"}>
+            <p className="first">
+              <img src="/image/img1.png" alt="" />
+              <span>BitCoin</span>
+            </p>
+            <p>₹ 21,39,962.21</p>
+            <p className="green">0.12%</p>
+            <p>₹ 511,300,246,257</p>
+          </Link>
+        </div>
+        <span></span>
+      </div>
+    </div>
+  );
 }
 
 export default Market;
--- src/pages/Coin/Coin.js
+++ src/pages/Coin/Coin.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Coin.scss";
 function Coin() {
-  return <div></div>;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div className="coin">
+      <div className="left">
+        <img src="/image/img1.png" alt="" />
+        <h2>Bitcoin</h2>
+        <p>Rank: #1</p>
+      </div>
+      <div className="right">
+        <div className="top">
+          <span>
+            24h Change:
+            <div className="green">1.85% </div>
+          </span>
+          <span>
+            Price:
+            <div className="green">$26,216.00</div>
+          </span>
+          <span>Symbol: btc</span>
+        </div>
+        <div className="bottom">
+          Bitcoin is the first successful internet money based on peer-to-peer
+          technology; whereby no central bank or authority is involved in the
+          transaction and production of the Bitcoin currency. It was created by
+          an anonymous individual/group under the name, Satoshi Nakamoto. The
+          source code is available publicly as an open source project, anybody
+          can look at it and be part of the developmental process. Bitcoin is
+          changing the way we see money as we speak. The idea was to produce a
+          means of exchange, independent of any central authority, that could be
+          transferred electronically in a secure, verifiable and immutable way.
+          It is a decentralized peer-to-peer internet currency making mobile
+          payment easy, very low transaction fees, protects your identity, and
+          it works anywhere all the time with no central authority and banks.
+          Bitcoin is designed to have only 21 million BTC ever created, thus
+          making it a deflationary currency. Bitcoin uses the SHA-256 hashing
+          algorithm with an average transaction confirmation time of 10 minutes.
+          Miners today are mining Bitcoin using ASIC chip dedicated to only
+          mining Bitcoin, and the hash rate has shot up to peta hashes. Being
+          the first successful online cryptography currency, Bitcoin has
+          inspired other alternative currencies such as Litecoin, Peercoin,
+          Primecoin, and so on. The cryptocurrency then took off with the
+          innovation of the turing-complete smart contract by Ethereum which led
+          to the development of other amazing projects such as EOS, Tron, and
+          even crypto-collectibles such as CryptoKitties.
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Coin;
